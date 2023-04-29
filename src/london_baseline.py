11bb3efa0b15109efb3fc2059f01d730489d924d
Tom <tmsilkowski@gmail.com>
2023-04-29 15:47:40 +0200

    Implemented london_baseline.py

--- src/london_baseline.py
+++ src/london_baseline.py
@@ -2,3 +2,14 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+
+import utils
+
+
+evaluation_corpus_path = 'birth_dev.tsv'
+with open(evaluation_corpus_path, 'r') as f:
+    input_lines_total = len(f.readlines())
+
+predictions = ['London'] * input_lines_total
+total, correct = utils.evaluate_places(evaluation_corpus_path, predictions)
+print(f'Correct: {correct} out of {total}: {correct/total*100}')
