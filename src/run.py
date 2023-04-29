2f99b174dcfff5ba7ab03f7be823d9910f0c7c22
Tom <tmsilkowski@gmail.com>
2023-04-29 18:19:49 +0200

    Completed 3f

--- src/run.py
+++ src/run.py
@@ -109,7 +109,16 @@ if __name__ == '__main__':
         #     warmup_tokens=512*20
         #     final_tokens=200*len(pretrain_dataset)*block_size
         #     num_workers=4
-        raise NotImplementedError
+        tconf = trainer.TrainerConfig(max_epochs=650,
+                                      batch_size=128,
+                                      learning_rate=6e-3,
+                                      lr_decay=True,
+                                      warmup_tokens=512*20,
+                                      final_tokens=200*len(pretrain_dataset)*block_size,
+                                      num_workers=4)
+        pretrain_trainer = trainer.Trainer(gpt_model, pretrain_dataset, None, tconf)
+        pretrain_trainer.train()
+        torch.save(gpt_model.state_dict(), args.writing_params_path)
     elif args.function == 'finetune':
         assert args.writing_params_path is not None
         assert args.finetune_corpus_path is not None
